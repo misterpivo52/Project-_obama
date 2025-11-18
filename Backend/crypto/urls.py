560c71552f17947dda1f319842a246ac20e955ca
zeroche22 <manzhos.yaroslav22@gmail.com>
2025-11-18 21:30:56 +0200

    done open api documentation

--- Backend/crypto/urls.py
+++ Backend/crypto/urls.py
@@ -19,29 +19,14 @@ from django.urls import path, include
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
-    SpectacularRedocView,
 )
-from rest_framework.permissions import AllowAny
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('users.urls')),
     path('api/', include('api.urls')),
     path("user/", include("users.urls")),
-    path(
-        "api/schema/",
-        SpectacularAPIView.as_view(permission_classes=[AllowAny]),
-        name="schema",
-    ),
-    path(
-        "api/docs/swagger/",
-        SpectacularSwaggerView.as_view(url_name="schema", permission_classes=[AllowAny]),
-        name="swagger-ui",
-    ),
-    path(
-        "api/docs/redoc/",
-        SpectacularRedocView.as_view(url_name="schema", permission_classes=[AllowAny]),
-        name="redoc",
-    ),
-
+    path ( 'api/schema/' , SpectacularAPIView . as_view (), name= 'schema' ),
+    path ( 'api/docs/' , SpectacularSwaggerView . as_view (url_name= 'schema' )),
 ]
